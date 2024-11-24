600086bc8b7b29398e0a5996f4e5b0357267dc9b
予我心安A3 <1958522865@qq.com>
2024-11-24 20:02:30 +0800

    feature:创建BaseController

--- Server/CTF_Team_Working_Platform/SystemControllers/BaseController.cs
+++ Server/CTF_Team_Working_Platform/SystemControllers/BaseController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CTFPlatForm.Application.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -7,38 +9,29 @@ using System.Text;
 
 namespace CTFPlatForm.Application.SystemControllers
 {
-    [Route("API/[controller]/[action]")]
     [ApiController]
+    [Route("[controller]")]
     public class BaseController : ControllerBase
     {
-        /// <summary>
-        /// 生成 JWT
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="issuer"></param>
-        /// <param name="audience"></param>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public string GenerateJwtToken(string userId, string issuer, string audience, string key)
+        protected readonly IConfiguration _configuration;
+        protected readonly ILogger<BaseController> _logger;
+        public BaseController(IConfiguration configuration, ILogger<BaseController> logger)
         {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            _configuration = configuration;
+            _logger = logger;
+        }
 
-            var claims = new[]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            // 验证用户凭据
+            if (model.Username == "validUser" && model.Password == "validPassword")
             {
-                new Claim(JwtRegisteredClaimNames.Sub, userId),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
+                var token = ToolHelper.GenerateJwtToken(model.Username, _configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], _configuration["Jwt:Key"]);
+                return Ok(new { token });
+            }
 
-            var token = new JwtSecurityToken(
-                issuer: issuer,
-                audience: audience,
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            return Unauthorized();
         }
 
     }
