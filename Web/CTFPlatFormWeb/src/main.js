5a5de3f209b1ad9af4e3254e8bd46330276c6b0a
予我心安A3 <1958522865@qq.com>
2025-01-04 15:07:06 +0800

    feature:首页 用户信息面板 demo

--- Web/CTFPlatFormWeb/src/main.js
+++ Web/CTFPlatFormWeb/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-
-const app = createApp(App);
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(ElementPlus);
 app.mount('#app');
